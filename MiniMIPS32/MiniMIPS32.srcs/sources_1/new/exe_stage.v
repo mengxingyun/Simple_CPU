c57e4b8b5a15868ee2c9947fb0e69d401c1e8a3a
mengxingyun <992735137@qq.com>
2020-10-21 10:58:57 +0800

    lui

--- MiniMIPS32/MiniMIPS32.srcs/sources_1/new/exe_stage.v
+++ MiniMIPS32/MiniMIPS32.srcs/sources_1/new/exe_stage.v
@@ -39,7 +39,8 @@ module exe_stage (
     // 根据内部操作码aluop进行逻辑运算
     assign logicres = (cpu_rst_n == `RST_ENABLE)  ? `ZERO_WORD : 
                       (exe_aluop_i == `MINIMIPS32_AND )  ? (exe_src1_i & exe_src2_i) : 
-                      (exe_aluop_i == `MINIMIPS32_ORI )  ? (exe_src1_i | exe_src2_i) : `ZERO_WORD;
+                      (exe_aluop_i == `MINIMIPS32_ORI )  ? (exe_src1_i | exe_src2_i) : 
+                      (exe_aluop_i == `MINIMIPS32_LUI )  ?  exe_src2_i : `ZERO_WORD;
             
     // 根据内部操作码aluop进行算术运算
     
--- MiniMIPS32/MiniMIPS32.srcs/sources_1/new/id_stage.v
+++ MiniMIPS32/MiniMIPS32.srcs/sources_1/new/id_stage.v
@@ -48,17 +48,18 @@ module id_stage(
     /*-------------------- 第一级译码逻辑：确定当前需要译码的指令 --------------------*/
     wire inst_reg  = ~|op;
     wire inst_and  = inst_reg& func[5]&~func[4]&~func[3]& func[2]&~func[1]&~func[0];
-    wire inst_mult = inst_reg & ~func[5] & func[4] & func[3] & ~func[2] & ~func[1] & ~func[0];//
+    wire inst_mult = inst_reg & ~func[5] & func[4] & func[3] & ~func[2] & ~func[1] & ~func[0];// 1: mult
     wire inst_mfhi = inst_reg & ~ func[5] & func[4] & ~func[3] & ~func[2] & ~func[1] & ~func[0];//2: move from hi
     wire inst_mflo = inst_reg & ~func[5] & func[4] & ~func[3] & ~func[2] & func[1] & ~func[0];//3: move from lo
     wire inst_sll  = inst_reg & ~func[5] & ~func[4] & ~func[3] & ~func[2] & ~func[1] & ~func[0];//4: sll
     wire inst_ori  = ~op[5] & ~op[4] & op[3] & op[2] & ~op[1] & op[0]; //5: ori
+    wire inst_lui  = ~op[5] & ~op[4] & op[3] & op[2] & op[1] & op[0];  //6: lui
     /*------------------------------------------------------------------------------*/
 
     /*-------------------- 第二级译码逻辑：生成具体控制信号 --------------------*/
     // 操作类型alutype
     assign id_alutype_o[2] = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : inst_sll;
-    assign id_alutype_o[1] = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mfhi | inst_mflo | inst_ori);
+    assign id_alutype_o[1] = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mfhi | inst_mflo | inst_ori | inst_lui);
     assign id_alutype_o[0] = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_mfhi | inst_mflo);
 
     // 内部操作码aluop
@@ -67,12 +68,12 @@ module id_stage(
     assign id_aluop_o[5]   = 1'b0;
     assign id_aluop_o[4]   = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mult | inst_sll | inst_ori);
     assign id_aluop_o[3]   = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mflo | inst_mfhi | inst_ori);
-    assign id_aluop_o[2]   = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mult | inst_mfhi | inst_mflo | inst_ori);
+    assign id_aluop_o[2]   = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mult | inst_mfhi | inst_mflo | inst_ori | inst_lui);
     assign id_aluop_o[1]   = 1'b0;
-    assign id_aluop_o[0]   = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_mflo | inst_sll | inst_ori);
+    assign id_aluop_o[0]   = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_mflo | inst_sll | inst_ori | inst_lui);
 
     // 写通用寄存器使能信号
-    assign id_wreg_o       = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mfhi | inst_mflo | inst_sll | inst_ori);
+    assign id_wreg_o       = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mfhi | inst_mflo | inst_sll | inst_ori | inst_lui);
     
     //写HILO寄存器使能信号
     assign id_whilo_o = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : inst_mult;
@@ -81,13 +82,16 @@ module id_stage(
     wire shift = inst_sll;
     
     //立即数使能信号
-    wire immsel = inst_ori;
+    wire immsel = inst_ori | inst_lui;
     
     //目的寄存器选择信号(rt还是rd)
-    wire rtsel = inst_ori;
+    wire rtsel = inst_ori | inst_lui;
     
     //符号扩展使能信号
     wire sext = 1'b0;
+    
+    //加载高半字使能信号
+    wire upper = inst_lui;
     
     // 读通用寄存器堆端口1使能信号
     assign rreg1 = (cpu_rst_n == `RST_ENABLE) ? 1'b0 : (inst_and | inst_mult | inst_ori);
@@ -101,7 +105,8 @@ module id_stage(
     assign ra2   = (cpu_rst_n == `RST_ENABLE) ? `ZERO_WORD : rt;
     
     //获得指令操作所需要的立即数
-    wire [31:0] imm_ext = (cpu_rst_n == `RST_ENABLE) ? `ZERO_WORD :
+    wire [31:0] imm_ext = (cpu_rst_n == `RST_ENABLE) ? `ZERO_WORD : 
+                           (upper == `UPPER_ENABLE) ? (imm << 16) :
                            (sext == `SIGNED_EXT) ? {{16{imm[15]}}, imm} : {{16{1'b0}}, imm};       
                                             
     // 获得待写入目的寄存器的地址（rt或rd）
